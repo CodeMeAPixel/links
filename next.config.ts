43b88b0b04b53c318a15d3bfb976651f80fb70bd
Pixelated <hey@codemeapixel.dev>
2026-02-09 16:37:32 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -17,17 +17,6 @@ const nextConfig: NextConfig = {
       }
     ]
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(mp3)$/,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/chunks/[path][name].[hash][ext]'
-      }
-    });
-
-    return config;
-  }
 };
 
 export default nextConfig;
